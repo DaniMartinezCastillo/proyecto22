ce8deede45769b9075d784be9e58e67feaadd7c7
JOSÉ DANIEL MARTÍNEZ CASTILLO <JoseDaniel.Martinez2@alu.uclm.es>
2023-01-14 19:09:32 +0100

    google3

--- servidor/passport-setup.js
+++ servidor/passport-setup.js
@@ -16,7 +16,7 @@ passport.use(new GoogleStrategy({
     clientID:"315556153013-q20nogk2esdv4o2u7ecap6lobmdjtpnd.apps.googleusercontent.com",
     clientSecret:"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",
     //callbackURL: "http://localhost:3000/"
-    callbackURL:"https://proyecto22-2ivjiwcgua-no.a.run.app/auth/google/callback"
+    callbackURL:"https://proyecto22-2ivjiwcgua-no.a.run.app/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     //User.findOrCreate({ googleId: profile.id }, function (err, user) {
